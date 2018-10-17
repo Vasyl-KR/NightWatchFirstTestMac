86b56b5e2ce22bcdeba2240e9205e990fa04adb8
Vasyl Kyrychuk <vkyry@macmini8556.ddns.softservecom.com>
2018-10-17 15:35:37 +0300

    Mac test

---
+++ tests/FirstTest.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    'Demo test Google' : function (browser) {
+
+        var google = browser.page.googlePage();
+
+
+        google.navigate()
+            .assert.title('Google')
+            .assert.visible('@searchBar')
+            .setValue('@searchBar', 'RetailMeNot')
+            .click('@submit');
+
+        browser.end();
+
+    }
+
+};
